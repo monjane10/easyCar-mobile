40a302ff1fb3e42841cdb1ee6cb380fdf056b30a
monjane10 <louramonja52.n@gmail.com>
2025-02-25 16:28:48 +0200

    adicionando a segunda tela

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,11 +1,13 @@
 import { Text } from "react-native"
 function Home() {
+
+
     return (
     <>
     
     <Text style={{fontSize: 30}}>Logotipo</Text>
-    <Text style={{fontSize: 30}}>Botao Passageiros</Text>
-    <Text style={{fontSize: 30}}>Botao Motoristas</Text>
+    <Text style={{fontSize: 30}}>Passageiros</Text>
+    <Text style={{fontSize: 30}}>Motoristas</Text>
 
     </>
     );
---
+++ src/screens/passenger/passenger.jsx
@@ -0,0 +1,17 @@
+import { Text } from "react-native"
+
+
+function Passenger() {
+    return (
+    <>
+    
+    <Text style={{fontSize: 30}}>Tela Passageiro</Text>
+    <Text style={{fontSize: 30}}>Mapa</Text>
+    <Text style={{fontSize: 30}}>Destino</Text>
+
+    </>
+    );
+}
+
+
+export default Passenger;
