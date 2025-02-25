0686b5b660f3837d6e1e14beb6d6c7600146285f
monjane10 <louramonja52.n@gmail.com>
2025-02-25 16:11:15 +0200

    dando inicio a tela 1

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    
+    <>
       <Text>TxovaGo</Text>
     
+    </>
   );
 }
 
