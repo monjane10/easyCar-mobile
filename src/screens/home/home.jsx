f011873985eb3f53fbf0350248153ccc1c8c2a56
monjane10 <louramonja52.n@gmail.com>
2025-02-26 09:51:45 +0200

    Finalizando a tela principal

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,4 +1,4 @@
-import { ImageBackground,Image, Text } from "react-native"
+import { ImageBackground, Image, Text, TouchableOpacity } from "react-native"
 import icons from "../../constants/icons.js";
 import { styles } from "./home.style.js";
 function Home() {
@@ -8,6 +8,20 @@ function Home() {
         <ImageBackground source={icons.bg} resizeMode="cover" style={styles.bg}>
 
             <Image source={icons.logo} style={styles.logo} />
+            <TouchableOpacity style={styles.btn}>
+                <Image source={icons.passenger}
+                    style={styles.img} />
+                <Text style={styles.title}>Passageiro</Text>
+                <Text style={styles.text}>Encontre um táxi para ti</Text>
+            </TouchableOpacity>
+
+           
+            <TouchableOpacity style={styles.btn}>
+                <Image source={icons.driver}
+                    style={styles.img} />
+                <Text style={styles.title}>Motorista</Text>
+                <Text style={styles.text}>Forneça Serviços de Táxi</Text>
+            </TouchableOpacity>
 
         </ImageBackground>
     );
--- src/screens/home/home.style.js
+++ src/screens/home/home.style.js
@@ -9,5 +9,27 @@ export const styles = ({
     logo: {
         width: 200,
         height: 46,
+        margin: 30
+    },
+
+    img:{
+        width: 160,
+        height: 160,
+    },
+
+    title: {
+        fontSize: 22,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+
+    text: {
+        fontSize: 16,
+        color: 'white',
+    },
+
+    btn: {
+        alignItems: 'center',
+        margin: 30
     }
 });
