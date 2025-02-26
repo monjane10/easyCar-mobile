fa4a416e675b50e8fea489c36f6ca69808bc66b8
monjane10 <louramonja52.n@gmail.com>
2025-02-26 12:59:43 +0200

    Ajustes na tela do passageiro

--- src/screens/passenger/passenger.jsx
+++ src/screens/passenger/passenger.jsx
@@ -51,6 +51,11 @@ function Passenger(props) {
                     <Text >Destino</Text>
                     <TextInput style={styles.input} />
                 </View>
+
+                <View style={styles.footerFields}>
+                    <Text >Motorista</Text>
+                    <TextInput style={styles.input} />
+                </View>
             </View>
 
             <MyButton text="CONFIRMAR" theme="" />
