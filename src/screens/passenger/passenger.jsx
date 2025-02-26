1a7d0d40797ab4aa089b4cf02bdef9c7b3939718
monjane10 <louramonja52.n@gmail.com>
2025-02-26 11:34:43 +0200

    Adicionando o mapa na tela do passageiro

--- src/screens/passenger/passenger.jsx
+++ src/screens/passenger/passenger.jsx
@@ -1,18 +1,44 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import MyButton from "../../components/mybutton/mybutton.jsx";
+import MapView, { Marker ,PROVIDER_DEFAULT} from "react-native-maps";
+import {styles} from './passenger.style.js'
+import { useState } from "react";
+import icons from '../../constants/icons.js'
 
 
 
 function Passenger(props) {
+
+    const [myLocation, setMyLocation] = useState({
+        latitude:-25.929247, 
+        longitude: 32.585896
+    });
+
+
+
     return (
-    <>
+    <View style={styles.container}>
+        <MapView style={styles.map} 
+                  provider={PROVIDER_DEFAULT}
+                  initialRegion={{
+                    latitude: -25.929247,
+                    longitude: 32.585896,
+                    latitudeDelta: 0.04,   
+                    longitudeDelta: 0.04
+                  }}>
+        <Marker coordinate={{
+            latitude: -25.929247,
+            longitude: 32.585896,
+        }}
+        title="Hayati Monjane"
+        description="FPLM, 4001" 
+        image={icons.location} style={styles.marker} />
+
+        </MapView>
     
-    <Text style={{fontSize: 30}}>Tela Passageiro</Text>
-    <Text style={{fontSize: 30}}>Mapa</Text>
-    <Text style={{fontSize: 30}}>Destino</Text>
     <MyButton text="CONFIRMAR" />
 
-    </>
+    </View>
     );
 }
 
---
+++ src/screens/passenger/passenger.style.js
@@ -0,0 +1,19 @@
+
+export const styles = ({
+    container: {
+        flex: 1,
+        
+    },
+
+    map: {
+        flex: 1,
+        width: '100%'
+    },
+
+    marker: {
+        width: 60,
+        height: 60
+    }
+
+   
+});
