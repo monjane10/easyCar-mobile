1480280f9a2604ac430cb710cc444333836d1809
monjane10 <louramonja52.n@gmail.com>
2025-03-02 12:33:26 +0200

    Corrigindo bugs na listagem dos pedidos de corrida

--- src/screens/ride/ride.jsx
+++ src/screens/ride/ride.jsx
@@ -1,8 +1,8 @@
 import { Text, View, FlatList, TouchableOpacity, Image } from "react-native";
 import { styles } from "./ride.style";
-import{json_rides} from "../../constants/dados.js";
 import icons from "../../constants/icons.js";
 import { useEffect, useState } from "react";
+import { api, HandleError } from "../../constants/api.js";
 
 
 function Ride(props) {
@@ -19,7 +19,14 @@ function Ride(props) {
 
     async function RequestRides(){
         //Aceder a API para obter os dados das corridas
-        setRides(json_rides);
+        try {
+            const response = await api.get("/rides/drivers/" + userId);
+            if (response.data)
+                setRides(response.data);
+        } catch (error) {
+            HandleError(error);
+            props.navigation.goBack();
+        }
     }
 
     useEffect(() => {
@@ -38,7 +45,10 @@ function Ride(props) {
                         }>
 
                             <View style={styles.containerName}>
-                            <Image source={icons.car} style={styles.car} />
+                                {
+                                    item.driver_user_id === userId &&  <Image source={icons.car} style={styles.car} />
+                                }
+                           
                             <Text style={styles.name}>{item.passenger_name}</Text>
                             </View>
                             <Text style={styles.address}>Origem: {item.pickup_address}</Text>
